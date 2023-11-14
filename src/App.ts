e20cc9fe1de3b4e0b9a2aa9402b927e99c8bc956
Chukwu Stanley <stanleychukwu17@yahoo.com>
2023-11-14 15:48:55 +0100

    finished the connection to postgres and registering, logging in and logging out of user

--- src/App.ts
+++ src/App.ts
@@ -1,12 +1,15 @@
 //--START-- importation fo assets
 import express from 'express'
+import cors from 'cors'
 require('dotenv').config()
-const {graphqlHTTP} = require('express-graphql')
 const {ObjectId} = require('mongodb');
-const {connectToDb, getDb} = require('./db')
+const {graphqlHTTP} = require('express-graphql')
 
 import pool from './db' // postgres database
 import routes from './routes'
+const {connectToDb, getDb} = require('./db')
+
+import deserializeUser from './middleware/deserializeUser'
 
 // for the error logger
 import {log, errorLogger} from './logger'
@@ -17,35 +20,53 @@ import { get_the_line_where_this_error_occurred } from './functions/utils'
 const port = process.env.PORT || 4000
 const app = express();
 app.use(express.json());
+app.use(cors({
+    origin: process.env.FRONTEND_URL,
+    methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
+    credentials: true, // Allow cookies and HTTP authentication
+    optionsSuccessStatus: 204, // Set the status for successful preflight requests
+}))
 
-//* import {connectToDb, getDb} from './db'
+//* Middlewares
+app.use(deserializeUser); // checks to see if the user is a has a valid accessToken or refreshToken
 
 
-//--START-- here is how to use the logger to log both errors and information, we have .info, .error, .warn, and .debug methods
-// errorLogger.info({stanley:'my  name is stanley', lastName: 'my last name is chukwu'})
+//* how to log errors for debugging purposes
+const logError = () => {
+    // below is how to use the logger to log both errors and information, we have .info, .error, .warn, and .debug methods
+    errorLogger.info({stanley:'my  name is stanley', lastName: 'my last name is chukwu'})
 
-// const error = new Error('Error occurred').stack!; // this will help me to get the line where the error occurred, then we will use regular expression to capture only the information that wee need
-// const capturedErrorLine = get_the_line_where_this_error_occurred({errorMessage: error})
-// errorLogger.error({'lineNumber': capturedErrorLine}, 'see error message 2')
-//--END--
+    const error = new Error('Error occurred').stack!; // this will help me to get the line where the error occurred, then we will use regular expression to capture only the information that wee need
+    const capturedErrorLine = get_the_line_where_this_error_occurred({errorMessage: error})
+    errorLogger.error({'lineNumber': capturedErrorLine}, 'see error message 2')
+}
+// logError()
 
-//* opens connection to the mongodb database before listening for request
-let db: any
-connectToDb((err: any) => {
-    if (!err) {
-        // now we can start listening for events
-        // app.listen(port, () => {
-        //     console.log(`now listening to request from port ${port}`)
-        // })
-
-        // updates our database variable
-        db = getDb()
-    } else {
-        console.log(`we have an error, error: ${err}`)
+//* connect to the postgres database and then allow express to receive request
+pool.connect((err: any, client: any, release: () => void) => {
+    if (err) {
+        return console.log('Error connecting to the postgresSQL database, because: ', err.stack)
     }
+
+    app.listen(port, () => {
+        console.log(`now listening to request from port ${port}`)
+        routes(app)
+    })
+
+    release()
 })
 
+//* opens connection to the mongodb database before listening for request
+// let db: any
+// connectToDb((err: any) => {
+//     if (!err) {
+//         app.listen(port, () => {
+//             console.log(`now listening to request from port ${port}`)
+//         })
 
-// app.listen(port, () => {
-//     console.log(`now listening to request from port ${port}`)
+//         // updates our database variable
+//         db = getDb()
+//     } else {
+//         console.log(`we have an error, error: ${err}`)
+//     }
 // })
