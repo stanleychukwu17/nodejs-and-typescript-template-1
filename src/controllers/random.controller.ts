319e010d9d6b422b06db196752eb59370e39fc04
Chukwu Stanley <stanleychukwu17@yahoo.com>
2023-12-08 16:39:53 +0100

    round up all resovers

--- src/controllers/random.controller.ts
+++ src/controllers/random.controller.ts
@@ -2,6 +2,7 @@
 import { pool } from '../db'
 import {show_bad_message, show_good_message} from '../functions/utils'
 
+//*--start--games
 // creates a new game
 export async function add_a_new_game_to_the_db (title: string, dateReleased: string) {
     const good_msg = show_good_message()
@@ -22,8 +23,57 @@ export async function add_a_new_game_to_the_db (title: string, dateReleased: str
 // returns all of the games
 export async function get_all_the_games_on_db () {
     const qCheck = await pool.query("SELECT * from games order by id desc")
-    const res = qCheck.rows
-    console.log(res)
-
-    return res
+    return qCheck.rows
 }
+
+// get a game by using the game id
+export async function get_a_game_using_the_id(id:number) {
+    const qCheck = await pool.query("SELECT * from games where id = $1 limit 1", [id])
+    return qCheck.rows[0]
+}
+//*--end--games
+
+//*--start--authors
+// returns all of the games
+export async function get_all_the_authors_on_db () {
+    const qCheck = await pool.query("SELECT * from users order by id desc")
+    return qCheck.rows
+}
+
+// get a game by using the game id
+export async function get_a_author_using_the_id(id:number) {
+    const qCheck = await pool.query("SELECT * from users where id = $1 limit 1", [id])
+    return qCheck.rows[0]
+}
+//*--end--authors
+
+//*--start--reviews
+// creates a new game
+export type newReview = {
+    gameId: number,
+    authorId: number,
+    rating: number,
+    content: string,
+}
+export async function add_a_new_review_to_the_db ({gameId, authorId, rating, content}: newReview) {
+    const good_msg = show_good_message()
+    console.log({gameId, authorId, rating, content})
+
+    // create a new session and return it
+    const qIn = await pool.query("INSERT INTO game_reviews (game_id, author_id, rating, content, date_added) values ($1, $2, $3, $4, now()) RETURNING *", [gameId, authorId, rating, content])
+
+    return {...good_msg, ...qIn.rows[0]}
+}
+
+// returns all of the games
+export async function get_all_the_reviews_on_db () {
+    const qCheck = await pool.query("SELECT * from game_reviews order by id desc")
+    return qCheck.rows
+}
+
+// get a game by using the game id
+export async function get_a_review_using_the_id(id:number) {
+    const qCheck = await pool.query("SELECT * from game_reviews where id = $1 limit 1", [id])
+    return qCheck.rows[0]
+}
+//*--end--reviews
--- src/graphql/resolvers.ts
+++ src/graphql/resolvers.ts
@@ -1,18 +1,56 @@
-import { add_a_new_game_to_the_db } from '../controllers/random.controller'
+import {
+    newReview,
+    add_a_new_game_to_the_db, get_all_the_games_on_db, get_a_game_using_the_id, get_all_the_authors_on_db, get_a_author_using_the_id, add_a_new_review_to_the_db, get_all_the_reviews_on_db,
+    get_a_review_using_the_id
+} from '../controllers/random.controller'
 type parent = {
     id: number
 }
 
 const resolvers = {
     Query : {
-        async game () {
-            // const allGames = 
-        }
+        async game (_:parent, {id}: {id: number}) {
+            return await get_a_game_using_the_id(id)
+        },
+
+        async games () {
+            return await get_all_the_games_on_db()
+        },
+
+        async author (_:parent, {id}: {id: number}) {
+            return await get_a_author_using_the_id(id)
+        },
+
+        async authors () {
+            return await get_all_the_authors_on_db()
+        },
+
+        async review (_:parent, {id}: {id: number}) {
+            return await get_a_review_using_the_id(id)
+        },
+
+        async reviews () {
+            return await get_all_the_reviews_on_db()
+        },
     },
     Mutation: {
         async createGame (_: parent, {title, date}: {title: string, date: string}) {
             const newGame = await add_a_new_game_to_the_db(title, date)
             return newGame
+        },
+
+        async createReview (
+                _: parent,
+                {info: {authorId, gameId, rating, content}}: {info: newReview}
+            ) {
+            const newReview = await add_a_new_review_to_the_db({authorId, gameId, rating, content})
+            return newReview
+        }
+    },
+
+    Game : {
+        reviews () {
+            
         }
     }
 }
